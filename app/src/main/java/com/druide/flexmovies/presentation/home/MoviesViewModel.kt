0c1474e32f89d278d40e7e2a850d10a1199596a8
mDjey <mehdi.djermoune11@gmail.com>
2022-07-22 09:13:56 +0200

    [+] add Resource class to handle liveData state

--- app/src/main/java/com/druide/flexmovies/presentation/home/MoviesViewModel.kt
+++ app/src/main/java/com/druide/flexmovies/presentation/home/MoviesViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.druide.flexmovies.common.Resource
 import com.druide.flexmovies.domain.model.Movies
 import com.druide.flexmovies.domain.model.Results
 import com.druide.flexmovies.domain.movies.MoviesUseCase
@@ -18,18 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MoviesViewModel  @Inject constructor( private val  moviesUseCase: MoviesUseCase) : ViewModel() {
 
-    private var _movies: MutableLiveData<Movies?> = MutableLiveData()
-    val movies: LiveData<Movies?> = _movies
-
-    private var _error: MutableLiveData<String> = MutableLiveData()
-    val error: LiveData<String> = _error
-
-    private var _canLoadMore: MutableLiveData<Boolean> = MutableLiveData()
-    val canLoadMore: LiveData<Boolean> = _canLoadMore
-
-    private var currentPage = -1
-
-    var fetchedList = listOf<Results>()
+    private var _moviesState: MutableLiveData<Resource<Movies>> = MutableLiveData()
+    val moviesState: LiveData<Resource<Movies>> = _moviesState
 
 
     /**
@@ -38,24 +29,21 @@ class MoviesViewModel  @Inject constructor( private val  moviesUseCase: MoviesUs
      * @param pageIndex Int by default the value should be 1
      */
     fun getMovieAtPage(pageIndex: Int) {
-        currentPage = pageIndex
+        _moviesState.postValue(Resource.Loading())
         viewModelScope.launch {
             val response = moviesUseCase.getPopularMovies(pageIndex)
 
             response.onSuccess {
                 if  ( this.statusCode.code == 200 && this.response.isSuccessful) {
-                    //fetchedList.addAll(data.results)
-                    _movies.value = data
-                    _canLoadMore.value = currentPage < data.totalPages
+                    _moviesState.value = Resource.Success(data)
                 }
             }
             response.onError {
-                _error.value =
-                    "${this.message()} [ Code : ${this.statusCode.code}], check your internet connection and retry"
+                _moviesState.value = Resource.Error(this.message())
             }
 
             response.onException {
-                _error.value = "Something wrong with : ${this.message()}"
+                _moviesState.value = Resource.Error(this.message())
             }
         }
     }
